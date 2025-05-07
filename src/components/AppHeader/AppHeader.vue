6af6f6b737b5306a4e0e3cc123824849460a9779
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-05-07 04:40:52 +0200

    feat(header): начата обработка заголовка, добавлен текстовой логотип

--- src/components/AppHeader/AppHeader.vue
+++ src/components/AppHeader/AppHeader.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import ContainerNav from "./ContainerNav.vue";
+</script>
+
 <template>
-  <header class="mock-up">AppHeader</header>
+  <header class="wrap-container">
+    <p>
+      <a href="#">JuJu Berry</a>
+    </p>
+    <ContainerNav />
+  </header>
 </template>
